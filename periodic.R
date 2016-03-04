4b5eb4285eb5797a287311c86da46e942936f251
Derik Castillo Guajardo <d.castillo@correo.ler.uam.mx>
2016-03-03 19:43:45 -0600

    Added loop for oscillations

--- periodic.R
+++ periodic.R
@@ -1,12 +1,11 @@
 #This file simulates an epidemiological model with 
-#periodic infection and recovery rates. 
+#periodic recovery rates. 
 #Research with Geiser
 
 library(deSolve)
 rm(list=ls())
 
-#define parameter values
-pars<-list(iota=0.01, mu=0.001, beta=20)
+
 
 #define initial conditions
 state<-c(susceptibles=0.4, infected=0.1, recovered=0)
@@ -22,12 +21,39 @@ epi<-function(t,state,pars)
   {
   periodic<-iota*(1+0.16*cos(2*pi*t+0.15))
   #here the ODEs
-    dx<- mu - mu*susceptibles - beta*susceptibles*infected
-    dy<- beta*susceptibles*infected - periodic*infected - mu*infected
-    dz<- periodic*infected - mu*recovered
-    return(list(c(dx,dy,dz)))
+    dsusceptibles<- mu - mu*susceptibles - beta*susceptibles*infected
+    dinfected<- beta*susceptibles*infected - periodic*infected - mu*infected
+    drecovered<- periodic*infected - mu*recovered
+    return(list(c(dsusceptibles,dinfected,drecovered)))
   })#this parenthesis closes the with
 }
-
-out<-ode(y=state,times=times,func=epi,parms = pars)
-plot(out)
+#do a for loop to explore parameter space
+#define sequences for three parameters, iota, mu and beta
+seqiota<-seq(from=0.0001,to=2,length.out = 100)
+seqmu<-seq(from=0.00001, to=0.2, length.out = 100)
+seqbeta<-seq(from=1, to=100, length.out = 100)
+parspace<-matrix(NA,nrow=1000000,ncol=4)
+colnames(parspace)<-c("iota","mu","beta","ampl")
+row<-0
+for (i in seqiota)
+{
+  for (m in seqmu)
+  {
+    for (b in seqbeta)
+    {
+      #define parameter values
+      pars<-list(iota=i, mu=m, beta=b)
+      out<-ode(y=state,times=times,func=epi,parms = pars)
+      row<-row+1
+      #plot(out,ylim=c(0.0044,0.0045),xlim=c(60,100))
+      
+      #get the amplitude in the infected
+      inf<-out[,2]
+      #shorten it to the last 50
+      long<-length(inf)
+      inf<-inf[(long-50):long]
+      ampl<-max(inf)-min(inf)
+      parspace[row,]<-c(unlist(pars),ampl=ampl)
+    }
+  }
+}
